9f28e92ab3f66e61cc0308b3a0418d2da7540bbf
opavelo <opavelo@gmail.com>
2021-08-15 17:23:53 +0300

    update

--- m2m-relations/articles/admin.py
+++ m2m-relations/articles/admin.py
@@ -6,14 +6,23 @@ from .models import Article, Scope
 
 class RelationshipInlineFormset(BaseInlineFormSet):
     def clean(self):
+        is_main = False
         for form in self.forms:
             # В form.cleaned_data будет словарь с данными
             # каждой отдельной формы, которые вы можете проверить
-            form.cleaned_data
+            if form.cleaned_data.get('is_main'):
+                if is_main:
+                    raise ValidationError('Уже есть is_main')
+                else:
+                    is_main = True
+
             # вызовом исключения ValidationError можно указать админке о наличие ошибки
             # таким образом объект не будет сохранен,
             # а пользователю выведется соответствующее сообщение об ошибке
-            #raise ValidationError('Тут всегда ошибка')
+            # raise ValidationError('Тут всегда ошибка')
+        if not is_main:
+            raise ValidationError('Нет is_main')
+
         return super().clean()  # вызываем базовый код переопределяемого метода
 
 
